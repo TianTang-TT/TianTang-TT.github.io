f572a10f9145f17db0e4d20be6782d21bfd1ea04
Honor <tiantang.honor@gmail.com>
2016-12-26 11:34:03 +0800

    update

---
+++ themes/yilia/source-src/js/main.js
@@ -0,0 +1,28 @@
+// 样式
+import '../css/main.scss'
+// 上报
+import './report'
+// fix hexo 不支持的配置
+import Fix from './fix'
+// 图片查看器
+import Viewer from './viewer'
+// 分享
+import Share from './share'
+// 浏览器判断
+import Browser from './browser'
+// 手机
+import Mobile from './mobile'
+// 异步脚本
+import { loadScript } from './util'
+
+window.onload = () => {
+	if(Browser.versions.mobile && window.screen.width < 800){
+		Mobile.init()
+	}else{
+		loadScript('/slider.js?v=4.0.0.js')
+	}
+	Fix.init()
+	Share.init()
+	Viewer.init()
+}
+
